d4f51244a87369980bfacf6158ff07c38db108f1
Cool Acid <coolacid217@gmail.com>
2022-11-30 13:54:17 -0600

    Saving array of points

--- wimote1.py
+++ wimote1.py
@@ -1,5 +1,9 @@
+import time
 import cwiid
 import matplotlib.pyplot as plt
+
+buffer_length = 100
+max_points = 500
 
 print('** Connecting wiimote **')
 wm = cwiid.Wiimote()
@@ -15,21 +19,40 @@ plt.show()    # now this should be non-blocking
 plt.plot(0, 0, markersize=0.1)
 plt.draw()
 plt.pause(0.001)
+fig = plt.figure(1)
+ax = fig.add_subplot()
+ax.axis([0,1000,0,800])
 
-# fig = plt.figure(1)
+xpoints = [0]
+ypoints = [0]
 
-xpoints = []
-ypoints = []
+plot, = ax.plot(xpoints, ypoints, color='black', markersize=1, marker='o', linestyle='')
+
+frames_buffer = 0
+should_draw = False
 
 while True:
   point1 = wm.state['ir_src'][0]
 
   if point1 is not None:
-    # print(point1)
     x,y = point1['pos']
-    size = point1['size']
-    # print(x, y)
+    xpoints.append(x)
+    ypoints.append(y)
 
-    plt.plot(x, y, color='black', marker='o', markersize=size)
-    plt.draw()
-    plt.pause(0.001)
+    if len(xpoints) > max_points:
+      xpoints.pop(0)
+      ypoints.pop(0)
+
+    should_draw = frames_buffer % buffer_length == 0 and frames_buffer != 0
+    frames_buffer += 1
+
+    if (should_draw):
+      frames_buffer = 0
+      plot.set_data(xpoints, ypoints)    
+      plt.draw()
+      plt.pause(0.001)
+
+    # print(f'points: {len(xpoints)}, buffer: {frames_buffer}, draw: {should_draw}, firstpoint: {xpoints[0]},{ypoints[0]}')
+
+  time.sleep(0.001)
+
