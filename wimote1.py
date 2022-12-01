1828c0c28331a88eeb269e130ec093bf82a4551a
Cool Acid <coolacid217@gmail.com>
2022-11-30 20:21:59 -0600

    ffs

--- wimote1.py
+++ wimote1.py
@@ -3,61 +3,92 @@ import cwiid
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy
+from multiprocessing import Process, Value, Array, Manager, Lock
 
-max_points = 500
-
-print('** Connecting wiimote **')
-wm = cwiid.Wiimote()
-wm.led = 15
-wm.rpt_mode = cwiid.RPT_IR
-print('** Connected **')
-
-plt.axis([0,1000,0,800])
-
-# plt.ion()     # turns on interactive mode
-
-fig = plt.figure(1)
-ax = fig.add_subplot()
-ax.axis([0,1000,0,800])
-
-xpoints = [50]
-ypoints = [50]
-
-plot, = ax.plot(xpoints, ypoints, color='black', markersize=1, marker='o', linestyle='')
-
-def update(point):
-  x,y = point
-
-  if x == 0 and y == 0: return plot
-
-  xps, yps = plot.get_data()
-
-  newXps = numpy.append(xps, x)
-  newYps = numpy.append(yps, y)
-
-  if len(newYps) > max_points:
-    newXps = numpy.delete(newXps, 0)        
-    newYps = numpy.delete(newYps, 0)      
-
-  plot.set_data(newXps, newYps)
-
-  return plot
-
-def getPoint():
+def test(xps, yps, lock):
   while True:
-    # st = time.time()
-    point = wm.state['ir_src'][0]
-    # print(time.time() - st)
+    lock.acquire()
+    xps.pop(0)
+    yps.pop(0)
+    xps.append(xps[len(xps) - 1] + 1)
+    yps.append(yps[len(yps) - 1] + 1)
+    lock.release()
+    # print('>', xps, ' ', yps)
+    time.sleep(0.05)
 
-    if point is None:
-      yield 0,0
-    else:
-      x,y = point['pos']
-      yield x,y
+def main():
+  with Manager() as manager:
+  
+    lock = Lock()
 
-anim = animation.FuncAnimation(fig, update, getPoint, interval=500)
+    max_points = 500
 
-plt.show()
+    xpoints = manager.list([1,2,3,4,5,6,7,8])
+    ypoints = manager.list([1,2,3,4,5,6,7,8])
+    
+    print('** Connecting wiimote **')
+    # wm = cwiid.Wiimote()
+    # wm.led = 15
+    # wm.rpt_mode = cwiid.RPT_IR
+    print('** Connected **')
+
+    plt.axis([0,1000,0,800])
+
+    fig = plt.figure(1)
+    ax = fig.add_subplot()
+    ax.axis([0,1000,0,800])
+
+    plot, = ax.plot(xpoints, ypoints, color='black', markersize=1, marker='o', linestyle='')
+
+    def update(point):
+      print('point', point)
+      # x,y = point
+
+      # if x == 0 and y == 0: return plot
+
+      # xps, yps = plot.get_data()
+
+
+
+      # newXps = numpy.append(xps, x)
+      # newYps = numpy.append(yps, y)
+
+      # if len(newYps) > max_points:
+      #   newXps = numpy.delete(newXps, 0)        
+      #   newYps = numpy.delete(newYps, 0)      
+
+      lock.acquire()
+      # print('<', xpoints, ' ', ypoints)
+      plot.set_data(xpoints, ypoints)
+      lock.release()
+
+      return plot
+
+    def getPoint():
+      while True:
+        # st = time.time()
+        # point = wm.state['ir_src'][0]
+        point = None
+        # print(time.time() - st)
+
+        if point is None:
+          yield 0,0
+        else:
+          x,y = point['pos']
+          yield x,y
+
+    p1 = Process(target=test, args=[xpoints, ypoints, lock])
+    
+    p1.start()
+    
+    anim = animation.FuncAnimation(fig, update, interval=10)
+
+    plt.show()
+
+    p1.join()
+
+if __name__ == '__main__':
+  main()
 # frames_buffer = 0
 # should_draw = False
 
